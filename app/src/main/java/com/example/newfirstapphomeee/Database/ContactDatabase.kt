1874edee57e7037be9d946704d0260bfda70346a
LifeStly <tattonman001@gmail.com>
2020-08-27 22:25:04 +0700

    save

--- app/src/main/java/com/example/newfirstapphomeee/Database/ContactDatabase.kt
+++ app/src/main/java/com/example/newfirstapphomeee/Database/ContactDatabase.kt
@@ -1,9 +1,11 @@
 package com.example.newfirstapphomeee.Database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [ContactData::class], version = 1, exportSchema = false)
 abstract class ContactDatabase : RoomDatabase(){
     abstract val databaseDao: ContactDataDAO
 
